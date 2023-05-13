b86964b3fbf5c3380ff25d295b3a65e345dbdac2
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-05-14 02:28:05 +0900

    Add data validation for user post api

--- fb-function/functions/controllers/user.js
+++ fb-function/functions/controllers/user.js
@@ -1,14 +1,22 @@
+// fb settings
 const functions = require("firebase-functions");
-
-const express = require("express");
-const cors = require("cors");
-const authMiddleware = require("../middleware/auth");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const db = admin.firestore();
 
+// express - rest api
+const express = require("express");
+
+// middleware for auth Token
+const authMiddleware = require("../middleware/auth");
+
+// set cors all access
+const cors = require("cors");
 const userApp = express();
 userApp.use(cors({origin: true}), authMiddleware);
+
+// data validation
+const {body, validationResult} = require("express-validator");
 
 userApp.get("/", async (req, res) => {
   const snapshot = await db.collection("users").get();
@@ -33,11 +41,21 @@ userApp.get("/:id", async (req, res) => {
   res.status(200).send(JSON.stringify({id: userId, ...userData}));
 });
 
-userApp.post("/", async (req, res) => {
+const userCreationValidators = [
+  body("email").notEmpty().isEmail(),
+  body("firstName").notEmpty(),
+  body("lastName").notEmpty(),
+  body("age").notEmpty().isInt(),
+  body("password").notEmpty().isLength({min: 6}),
+];
+
+userApp.post("/", userCreationValidators, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
   const user = req.body;
-
   await db.collection("users").add(user);
-
   res.status(201).send();
 });
 userApp.put("/:id", async (req, res) => {
